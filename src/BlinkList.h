41cfa816d75a8a1b85f576b931560075123ff3b8
balmistan <sangiovanni.daniele@gmail.com>
2025-04-22 10:44:43 +0200

    Aktualisieren von BlinkList.h

--- src/BlinkList.h
+++ src/BlinkList.h
@@ -20,8 +20,8 @@ public:
     BlinkList();                 // Constructor
     ~BlinkList();                // Destructor
 
-    void add(uint8_t gpio);         // Add a value to the end
-    bool search(uint8_t gpio);      // Search for a value
+    Node* add(uint8_t gpio);         // Add a value to the end
+    Node* search(uint8_t gpio);      // Search for a value
     bool remove(uint8_t gpio);      // Remove a node by value
     void print();                // Print all elements (for debug)
 };
