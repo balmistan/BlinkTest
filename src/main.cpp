872180b237efd08f3dda4f9ed396d65a8bca74c5
balmistan <sangiovanni.daniele@gmail.com>
2025-05-02 22:11:26 +0200

    main.cpp updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,16 @@
 #include <Arduino.h>
-#include "BlinkList.h"
+#include "blist.h"
 
-BlinkList list;
+#define GPIOOUT 5
+
+BList list;
 
 void setup() {
     Serial.begin(9600);
+    pinMode(GPIOOUT, OUTPUT);
 
-    list.add(42);
-    list.add(7);
-    list.add(13);
-
-    Serial.println("BlinkList contents:");
-    list.print();
-
-    if (list.search(7)) {
-        Serial.println("Found 7!");
-    }
-
-    list.remove(42);
-    Serial.println("After removing 42:");
-    list.print();
+    list.configureBlink(GPIOOUT, 1000, 1000);
+    list.startBlink(GPIOOUT);
 }
 
 void loop() {
