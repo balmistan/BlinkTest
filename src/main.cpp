8fd1f4389c486340a4dbba721656a4d7d9530c9a
balmistan <sangiovanni.daniele@gmail.com>
2025-05-03 05:32:03 +0200

    Aktualisieren von main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,12 @@ BList list;
 void setup() {
     Serial.begin(9600);
     pinMode(GPIOOUT, OUTPUT);
-
+      timer.attach_ms(10, onTimer);  // Call onTimer every 10 ms
     list.configureBlink(GPIOOUT, 1000, 1000);
     list.startBlink(GPIOOUT);
 }
+
+
 
 void loop() {
     // Your loop logic here
